9b9875b2d515969907cf3d1cf890de100b93a22a
Filli4 <145070135+Filli4@users.noreply.github.com>
2024-01-20 23:20:15 +0100

    Update radioplayer.js

--- radioplayer.js
+++ radioplayer.js
@@ -27,28 +27,3 @@ getRadioChannel();
 // <audio controls>
 //   <source src="" type="audio/mpeg" />
 // </audio>
-/*async function getRadioChannels() {
-    const response = await fetch(
-      "https://api.sr.se/api/v2/channels/?format=json&size=4"
-    );
-    const data = await response.json();
-  
-    console.log("data", data);
-  
-    const channelDiv = document.querySelector("#channels");
-  
-    data.channels.forEach((channel) => {
-      const div = document.createElement("div");
-      div.id = "test";
-  
-      div.innerHTML = `
-      <img src="${channel.image}" alt="${channel.name}" /> <h2>${channel.name}</h2>
-      <audio controls> 
-      <source src="${channel.liveaudio.url}" type="audio/mpeg" />" 
-      </audio>`;
-  
-      channelDiv.appendChild(div);
-    });
-  }
-  
-  getRadioChannels(); */
